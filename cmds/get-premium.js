a79a62eecdd4b1d0d7a1068e2b35687f70ab998e
Clemens Ertle <clemensertle@gmail.com>
2018-09-22 18:43:06 +0200

    command to set a guild to premium.

---
+++ cmds/get-premium.js
@@ -0,0 +1,14 @@
+const $console = require('Console');
+module.exports.run = async (client, message) => {
+    if (!client.reactsave.has(message.guild.id) || !client.settings.has(message.guild.id)) {
+        message.author.send(':x: Something unexpected happened\nThe developer got a notification');
+        message.channel.send(':x: Something unexpected happened\nThe developer got a notification');
+        $console.stress('data for a server was not available');
+        $console.error(`message.guild: ${message.guild.name}|${message.guild.id}\nreactsave: ${client.reactsave.has(message.guild.id)}\nsettings: ${client.settings.has(message.guild.id)}`);
+        return;
+    }
+    if (!client.premium.has(message.author.id)) return message.reply('You are not listed in the Premium Database. Contact CHY4E#0505 if you think you should be');
+    if (!message.member.permissions.has('ADMINISTRATOR')) return message.reply('You need to be Administrator on thisguild.\n(so you can\' give every guild premium');
+    client.settings.setProp(message.guild.id, 'premium', true);
+    message.channel.send(`The guild \`${message.guild.name}\` is now Premium`);
+};
