fc702706abd61b404a6fc1bb589e36d5c47ca7ea
Clemens Ertle <clemensertle@gmail.com>
2018-09-30 01:25:52 +0200

    doesnt work yet

--- cmds/setup.js
+++ cmds/setup.js
@@ -3,3 +3,10 @@ module.exports.run = async (client, message, args) => {
 
 
 };
+
+exports.help = {
+    name: 'setup',
+    category: 'settings',
+    example: 'coming',
+    description: 'coming',
+};
