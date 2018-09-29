2fdf2da96557034433aa8801dfb9f7f1f1ac7a7f
Clemens Ertle <clemensertle@gmail.com>
2018-09-30 01:25:31 +0200

    trying to fetch the invite.

--- modules/invite_delete.js
+++ modules/invite_delete.js
@@ -2,9 +2,11 @@ const Discord = require('discord.js');
 module.exports.run = async (client, message) => {
     if (message.member.highestRole.comparePositionTo(message.guild.me.highestRole) >= 0) return;
     const re = new RegExp('(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite)\/.+[a-z]');
-    if (!re.test(message.content)) { return; }
+    if (!message.content.match(re)) return;
     if (!message.channel.permissionsFor(message.guild.me).has('MANAGE_MESSAGES')) return;
-
+    const match = re.exec(message.content);
+    const invite = await client.fetchInvite(match[0]).catch((O_o) => O_o);
+    if (invite.guild && message.guild.name === invite.guild.name) return;
     await message.delete();
     const logchannel = client.settings.getProp(message.guild.id, 'log_channel');
     if (!client.channels.has(logchannel)) return;
