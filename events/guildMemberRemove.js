8871b004eb77843e2c1402d496dac1c61cab1a4d
Clemens Ertle <clemensertle@gmail.com>
2018-10-04 22:04:46 +0200

    replace string

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -4,7 +4,7 @@ module.exports = async (client, member) => {
     if (!client.channels.has(greetchannel) || !client.channels.get(greetchannel).permissionsFor(member.guild.me).has('SEND_MESSAGES')) return;
     let wmsg = client.settings.getProp(member.guild.id, 'welcome_m');
     if (wmsg == '') {
-        const premade = ['Good Bye {user}', '{user} left {guild}, press F to pay respect', '{user} left {guild}', '{user} left', '{user} left. We will miss him. maybe'];
+        const premade = ['goodbye {user}', '{user} left {guild}, press F to pay respect', '{user} left {guild}', '{user} left', '{user} left. We will miss him. maybe'];
         wmsg = premade[Math.floor(Math.random() * premade.length)];
     }
     wmsg = wmsg.replace('{user}', member.user.tag);
