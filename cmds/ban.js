618ad63a3b78e3a6a70dc5c2bca21f8dc5f2ce97
Clemens Ertle <clemensertle@gmail.com>
2018-10-06 17:02:23 +0200

    totally syntax fail

--- cmds/ban.js
+++ cmds/ban.js
@@ -6,19 +6,20 @@ module.exports.run = async (client, message, args) => {
     if (!member) return message.channel.send('Please mention the member you want to ban');
     if (member.highestRole.position >= message.member.highestRole.position) return message.channel.send('The mentioned members highest role position equals or exceeds yours.');
     if (!member.bannable) return message.channel.send('I can\'t ban that member');
-    if (member.highestRole.position >= message.guilargs.shift();
-    const reason = args.join(' ');d.me.highestRole.position) return message.channel.send('The mentioned members highest role position equals or exceeds mine.');
-    
+    if (member.highestRole.position >= message.guild.me.highestRole.position) return message.channel.send('The mentioned members highest role position equals or exceeds mine.');
+    args.shift();
+    const reason = args.join(' ');
+
     await member.ban(`${message.author} with reason: ${reason}`);
 
     message.channel.send(new Discord.RichEmbed()
         .setColor(3127860)
         .addField('User banned', `:white_check_mark: ${member.user.tag} was banned`));
-    const logchannel = client.settings.getProp(message.guild.id, 'log_channel');
+    const logchannel = client.getLogchannel(message.guild.id);
 
-    if (!client.channels.has(logchannel)) return message.reply(` __Warning:__ You don't have a log channel!\nPlease use \`${client.settings.getProp(message.guild.id, 'prefix')}loghere\` in the new log channel.`);
-    if (!client.channels.get(logchannel).permissionsFor(message.guild.me).has('SEND_MESSAGES')) return message.reply(' __Warning:__ I don\'t have the permission to send messages in your log channel.');
-    client.channels.get(logchannel).send(new Discord.RichEmbed()
+    if (!logchannel) return message.reply(` __Warning:__ You don't have a log channel!\nPlease use \`${client.settings.getProp(message.guild.id, 'prefix')}loghere\` in the new log channel.`);
+    if (!logchannel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) return message.reply(' __Warning:__ I don\'t have the permission to send messages in your log channel.');
+    logchannel.send(new Discord.RichEmbed()
         .setColor(3127860)
         .addField('User banned', `${message.author} banned ${member.user.tag} for \`${reason}\``));
 };
