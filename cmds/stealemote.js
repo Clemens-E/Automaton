2636deac1a08f0c556cb3e8689a69218dce2cae3
Clemens Ertle <clemensertle@gmail.com>
2018-10-13 21:09:00 +0200

    adding emotes by url

---
+++ cmds/stealemote.js
@@ -0,0 +1,17 @@
+module.exports.run = async (client, message, args) => {
+    if (!message.channel.permissionsFor(message.guild.me).has('SEND_MESSAGES')) return message.author.send(`I can't send messages in ${message.channel}.`);
+    if (!message.member.hasPermission('MANAGE_EMOJIS')) return message.channel.send('You don\'t have enough permissions to manage emotes');
+    if (!message.guild.me.permissions.has('MANAGE_EMOJIS')) return message.channel.send('I don\'t have enough permissions to manage emotes');
+    if (!args[0]) return message.channel.send('Please give in a url. See help for a example');
+    if (!args[1]) return message.channel.send('Please give in a name after the url. See help for a example');
+    message.guild.createEmoji(args[0], args[1])
+        .then(emoji => message.channel.send(`Done ${emoji}`))
+        .catch(err => message.channel.send(err.message));
+};
+
+exports.help = {
+    name: 'stealemote',
+    category: 'others',
+    example: 'stealemote https://cdn.discordapp.com/emojis/498513312569360387.gif?v=1 wow',
+    description: 'adds the linked emoji to the guild. add a name behind the url',
+};
