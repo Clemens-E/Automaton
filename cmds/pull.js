c4cb5080e669d337d496eb594ff52c74b0ed0a4c
Clemens Ertle <clemensertle@gmail.com>
2018-10-20 21:13:23 +0200

    pulling and restarting

---
+++ cmds/pull.js
@@ -0,0 +1,20 @@
+const child = require('child_process');
+module.exports.run = async (client, message) => {
+    if (message.author.id !== client.config.ownerid) return;
+    const msg = await message.channel.send('executing pull command...');
+    child.exec('git pull origin master', (err, stdout, stderr) => {
+        if (err) return msg.edit(err);
+        if (stdout === 'Already up-to-date.') return msg.edit(stdout);
+        msg.edit(stdout + '  Now restarting...');
+        client.settings.set('lastMessage', { msg: msg.id, channel: msg.channel.id });
+        process.exit(0);
+    });
+};
+
+
+exports.help = {
+    name: 'restart',
+    category: 'owner commands',
+    example: 'restart',
+    description: 'Stops the process. Hopefully pm2 restarts it',
+};
