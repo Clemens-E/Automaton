7eb08b40c1e7cd9e9d59273d9777d6e2c09edec1
Clemens Ertle <clemensertle@gmail.com>
2018-10-26 16:18:51 +0200

    nice emojis to spice it up

--- cmds/dehoist.js
+++ cmds/dehoist.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-module.exports.run = async (client, message, args) => {
+module.exports.run = async (client, message) => {
     const channel = message.channel;
     const msg = await channel.send('fetching members.');
     const guild = await message.guild.fetchMembers();
@@ -7,14 +6,14 @@ module.exports.run = async (client, message, args) => {
     if (!message.member.hasPermission('MANAGE_NICKNAMES') && message.author.id !== client.config.ownerid) return message.reply('You need the permission `Manage Nicknames`');
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.reply('I need the permission `Manage Nicknames`');
     const hoisted = guild.members.filter(m => !new RegExp(/^([a-z]|[0-9])/, 'i').test(m.displayName));
-    await msg.edit(`found ${hoisted.size} hoisted users.`);
+    await msg.edit(`found ${hoisted.size} hoisted users.\nnow dehoisting ${client.config.loading}`);
     let changed = 0;
     hoisted.forEach(element => {
         if (element.highestRole.position >= message.guild.me.highestRole.position) return;
         element.setNickname('no hoisting', `dehoisting runned by ${message.author.tag}`);
         changed++;
     });
-    msg.edit(`changed ${changed} Nicknames of ${hoisted.size} hoisted users.`);
+    setTimeout(msg.edit(`changed ${changed} Nicknames of ${hoisted.size} hoisted users.`), 1000);
 };
 
 exports.help = {
--- cmds/update.js
+++ cmds/update.js
@@ -1,13 +1,13 @@
 const { exec } = require('child_process');
 module.exports.run = async (client, message) => {
     if (message.author.id !== client.config.ownerid) return;
-    const msg = await message.channel.send('executing pull command...');
+    const msg = await message.channel.send(`${client.config.updating} executing pull command...`);
     exec('git pull origin master', async (err, stdout) => {
         if (err) throw err;
         if (stdout === 'Already up-to-date.\n') return msg.edit(stdout);
-        await msg.edit(`\`\`\`fix\n${stdout}\`\`\`Now restarting...`);
+        await msg.edit(`${client.config.updating}\`\`\`fix\n${stdout}\`\`\`Now restarting...`);
         client.settings.set('lastMessage', { msg: msg.id, channel: msg.channel.id, content: stdout });
-        console.log('updated code. restarting now');
+        console.log('updated code. restarting now ');
         process.exit(0);
     });
 };
