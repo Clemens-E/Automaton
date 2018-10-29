1e8407693e517a196c316c73230c753adfb70925
Clemens Ertle <clemensertle@gmail.com>
2018-10-29 01:08:45 +0100

    first check then fetch

--- cmds/dehoist.js
+++ cmds/dehoist.js
@@ -1,10 +1,11 @@
 module.exports.run = async (client, message) => {
     const channel = message.channel;
-    const msg = await channel.send(`${client.config.loading} fetching members.`);
     const guild = await message.guild.fetchMembers();
     if (!channel.permissionsFor(guild.me).has('SEND_MESSAGES')) return message.author.send(`I can't send messages in ${channel}. Please make sure I can and try again.`);
     if (!message.member.hasPermission('MANAGE_NICKNAMES') && message.author.id !== client.config.ownerid) return message.reply('You need the permission `Manage Nicknames`');
     if (!message.guild.me.hasPermission('MANAGE_NICKNAMES')) return message.reply('I need the permission `Manage Nicknames`');
+    const msg = await channel.send(`${client.config.loading} fetching members.`);
+
     const hoisted = guild.members.filter(m => !new RegExp(/^([a-z]|[0-9])/, 'i').test(m.displayName));
     await msg.edit(`found ${hoisted.size} hoisted users.\nnow dehoisting ${client.config.loading}`);
     let changed = 0;
