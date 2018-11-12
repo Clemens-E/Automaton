5ecbffda7a1f309db8f3ed54018c4374e10ec91d
CHY4E <clemensertle@gmail.com>
2018-11-12 07:24:23 +0100

    .find without a result returns a empty collection, not undefined

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -3,7 +3,7 @@ module.exports.run = async (client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag.toLowerCase().includes(args.join(' ').toLowerCase())) ||
         message.guild.members.find(m => m.displayName.toLowerCase().includes(args.join(' ').toLowerCase()));
     if (!args[0]) member = message.member;
-    if (!member) message.channel.send(`I can't find a member named ${args.join('')}`);
+    if (!member || member.size === 0) message.channel.send(`I can't find a member named ${args.join('')}`);
     const embed = new Discord.RichEmbed()
         .setTitle(member.user.username + '\' avatar')
         .setImage(member.user.displayAvatarURL)
