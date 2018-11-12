a39e883021a06e61551f7d91882055a5f4fcba80
Clemens Ertle <clemensertle@gmail.com>
2018-11-12 22:11:07 +0100

    returning if no member was found

--- cmds/avatar.js
+++ cmds/avatar.js
@@ -1,9 +1,8 @@
 const Discord = require('discord.js');
 module.exports.run = async (client, message, args) => {
-    let member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag.toLowerCase().includes(args.join(' ').toLowerCase())) ||
+    const member = message.mentions.members.first() || message.guild.members.find(m => m.user.tag.toLowerCase().includes(args.join(' ').toLowerCase())) ||
         message.guild.members.find(m => m.displayName.toLowerCase().includes(args.join(' ').toLowerCase()));
-    if (!args[0]) member = message.member;
-    if (!member || member.size === 0) message.channel.send(`I can't find a member named ${args.join('')}`);
+    if (!member) return message.channel.send(`I can't find a member named \`${args.join('')}\``);
     const embed = new Discord.RichEmbed()
         .setTitle(member.user.username + '\' avatar')
         .setImage(member.user.displayAvatarURL)
@@ -17,7 +16,7 @@ exports.help = {
     name: 'avatar',
     category: 'others',
     example: 'avatar Auto',
-    description: 'Showing the avatar of the mentioned user or seaches him by name',
+    description: 'Showing the avatar of the mentioned user or seaches him by name/nickname',
     userPermissions: [],
     userChannelPermissions: [],
     myPermissions: [],
