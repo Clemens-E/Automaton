10693cda5d7a25767007160a79f4292d654e531e
Clemens Ertle <clemensertle@gmail.com>
2019-01-04 19:41:05 +0100

    removing the highlight

--- modules/highlight.js
+++ modules/highlight.js
@@ -11,7 +11,7 @@ module.exports.run = async (client, message) => {
     // Get the last 3 messages in that channel.
     const lastMessages = message.channel.messages.array().slice(-3).filter((m) => !m.author.bot);
     const output = lastMessages.map((m) => `${m.author.tag}: ${(m.content.length > 100) ? `${m.content.substring(0, 100)}...` : m.content}`);
-    const embed = new Discord.RichEmbed().setColor(client.infos.ci).setDescription(`${message.member} wrote a [highlighted Word](${message.url}) in ${message.channel}\nChat History:\n\`\`\`fix\n${output.join('\n')}\`\`\``);
+    const embed = new Discord.RichEmbed().setColor(client.infos.ci).setDescription(`${message.member} wrote a [highlighted Word](${message.url}) in ${message.channel}\nChat History:\n\`\`\`\n${output.join('\n')}\`\`\``);
     const msg = await logchannel.send(embed.setFooter('React with 🗑 to delete the message (10 Minutes Time)'));
     await msg.react('🗑');
     const filter = (r, u) =>
